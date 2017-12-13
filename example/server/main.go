cf42d079a5594508a93da8d6f17be7e8ecd8ec31
Zac Garby <zacgarby@icloud.com>
2017-12-13 17:29:23 +0000

    Add an example of the server

---
+++ example/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Zac-Garby/ezgnl/server"
+)
+
+// A simple echo server
+func main() {
+	s := server.New("8080")
+	defer s.Close()
+
+	s.Accept(func(conn server.Conn, id server.UUID) {
+		conn.Handle("message", func(msg interface{}) {
+			log.Println("received message:", msg)
+			conn.Send("reply", msg)
+		})
+
+		conn.Disconnect(func(interface{}) {
+			log.Println("a user left")
+		})
+	})
+
+	log.Println("listening...")
+
+	if err := s.Listen("tcp"); err != nil {
+		log.Println("listen:", err)
+	}
+}
