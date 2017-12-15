cee4903945e4737937752263e9d837733d98f610
Zac Garby <zacgarby@icloud.com>
2017-12-15 17:39:26 +0000

    Add Client.Listen() method
    Connects to a server and starts listening for messages.

--- client/client.go
+++ client/client.go
@@ -20,6 +20,7 @@ type Client struct {
 	address string
 	conn    net.Conn
 	id      UUID
+	closed  bool
 
 	incoming, outgoing chan *message.Message
 
@@ -32,10 +33,36 @@ type Client struct {
 func New(addr string) *Client {
 	return &Client{
 		address:  addr,
+		closed:   true,
 		incoming: make(chan *message.Message),
 		outgoing: make(chan *message.Message),
 		handlers: make(map[string]MessageHandler),
 	}
+}
+
+// Listen dials the address specified in New() and attempts
+// to connect to the server.
+//
+// network is the network to connect to, such as "tcp" or "udp".
+func (c *Client) Listen(network string) error {
+	conn, err := net.Dial(network, c.address)
+	if err != nil {
+		return err
+	}
+
+	c.conn = conn
+	c.closed = false
+
+	for !c.closed {
+		msg, err := message.Receive(conn)
+		if err != nil {
+			return err
+		}
+
+		c.handleMessage(msg)
+	}
+
+	return nil
 }
 
 // Handle sets the message handler of a certain message type.
@@ -48,5 +75,11 @@ func (c *Client) Send(t string, data interface{}) {
 	c.outgoing <- &message.Message{
 		Type: t,
 		Data: data,
+	}
+}
+
+func (c *Client) handleMessage(msg *message.Message) {
+	if handler, ok := c.handlers[msg.Type]; ok {
+		handler(msg.Data)
 	}
 }
